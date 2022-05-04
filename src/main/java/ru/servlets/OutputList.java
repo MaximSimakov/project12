5ea2ea563f2a7bad450922d7b07901bde252d631
SimakovMaxim <simakov.maxim@inbox.ru>
2022-05-04 21:42:56 +0500

    updated

--- src/main/java/ru/servlets/OutputList.java
+++ src/main/java/ru/servlets/OutputList.java
@@ -1,12 +1,22 @@
 package ru.servlets;
 
-public class OutputList {
-	public void OL() {
+import java.io.IOException;
+
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class OutputList implements ServletContextListener {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 	if( request.getParameter( "sign5" ) != null ) {
+		@SuppressWarnings("unused")
 		String ollist = "1. Симаков М.В </br> 2. Асьянов А.А </br> 3. Рогова А.А </br>  4.Шайхутдинов Т.М";
 	}
-	
-	OL();
+
+	}
 	
 	}
-}
+
