6cec37e34c465de264df31430f5f657c96d01e2b
shaihutdinov <timur.shai@mail.ru>
2022-05-14 11:36:44 +0400

    try to fix encoding bug

--- src/main/java/rgrproject/main.java
+++ src/main/java/rgrproject/main.java
@@ -24,7 +24,7 @@ public class main extends HttpServlet{
 	
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		request.setCharacterEncoding("UTF-8");
 		CalculatorStateImpl data=new CalculatorStateImpl();
 		
 		try {
