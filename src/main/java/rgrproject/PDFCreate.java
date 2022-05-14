b7a2801639425d2e16751e3dfb4fa79bbab699d9
shaihutdinov <timur.shai@mail.ru>
2022-05-14 13:00:14 +0400

    change

--- src/main/java/rgrproject/PDFCreate.java
+++ src/main/java/rgrproject/PDFCreate.java
@@ -49,7 +49,7 @@ public class PDFCreate {
 			BaseFont times = null;
 			URL Imagelink=getClass().getResource("/pictures/UGATU.jpg");
 			try {
-				times = BaseFont.createFont("/wwebapps/Project_12/WEB-INF/classes/fonts/times.ttf", "cp1251", BaseFont.EMBEDDED);
+				times = BaseFont.createFont("/webapps/Project_12/WEB-INF/classes/fonts/times.ttf", "cp1251", BaseFont.EMBEDDED);
 			} catch (DocumentException e2) {
 				e2.printStackTrace();
 			} catch (IOException e2) {
